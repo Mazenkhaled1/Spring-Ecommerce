f9620139d5cd287cbebe46d0c835b5f62a3a4968
mazen khaled <khaledmazen64@gmail.com>
2026-06-01 08:43:05 +0300

    create dtos for fully crud user also finish the implementaion of the full crud on userController aslo testing

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -1,12 +1,18 @@
 package com.codewithmosh.store.controllers;
 
 
+import com.codewithmosh.store.dtos.ChangePasswordRequest;
+import com.codewithmosh.store.dtos.RegisterUserRequest;
+import com.codewithmosh.store.dtos.UpdateUserRequest;
 import com.codewithmosh.store.dtos.UserDto;
+import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Set;
 
@@ -42,5 +48,66 @@ public class UserController {
          return ResponseEntity.ok(userDto);
 
 
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(
+            @RequestBody RegisterUserRequest request,
+            UriComponentsBuilder uriBuilder) {
+
+        User user = new User();
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        userRepository.save(user);
+
+          var userDto = new  UserDto(user.getId() , user.getName() , user.getEmail());
+          var uri =  uriBuilder.path("/users/{id}").buildAndExpand(user.getId()).toUri();
+          return ResponseEntity.created(uri).body(userDto);
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDto> updateUser(@PathVariable(name = "id") Long id, @RequestBody UpdateUserRequest request)
+    {
+        var user = userRepository.findById(id).orElse(null);
+        if(user == null ) {
+            return ResponseEntity.notFound().build();
+        }
+
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+        userRepository.save(user);
+        var userDto = new  UserDto(user.getId() , user.getName() , user.getEmail());
+        return ResponseEntity.ok(userDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable(name = "id") Long id) {
+        var user = userRepository.findById(id).orElse(null);
+        if(user == null ) {
+            return ResponseEntity.notFound().build();
+        }
+        userRepository.delete(user);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    @PostMapping("/{id}/change-password")
+    public ResponseEntity<Void> changePassword(
+            @PathVariable(name = "id") Long id ,
+            @RequestBody ChangePasswordRequest request
+    )
+    {
+        var user = userRepository.findById(id).orElse(null);
+        if(user == null ) {
+            return ResponseEntity.notFound().build();
+        }
+        if(! user.getPassword().equals(request.getOldPassword())) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        user.setPassword(request.getNewPassword());
+        userRepository.save(user);
+        return ResponseEntity.noContent().build();
     }
 }
