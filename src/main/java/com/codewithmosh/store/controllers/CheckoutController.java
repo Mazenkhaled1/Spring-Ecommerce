7ecca313bb6cc63fc58269a3cbd9f14f8e54583f
mazen khaled <khaledmazen64@gmail.com>
2026-06-23 15:45:50 +0300

    decoupling from stripePayment and refactor the code of the stripePayment

--- src/main/java/com/codewithmosh/store/controllers/CheckoutController.java
+++ src/main/java/com/codewithmosh/store/controllers/CheckoutController.java
@@ -5,6 +5,7 @@ import com.codewithmosh.store.dtos.CheckoutResponse;
 import com.codewithmosh.store.dtos.ErrorDto;
 import com.codewithmosh.store.exceptions.CartEmptyException;
 import com.codewithmosh.store.exceptions.CartNotFoundException;
+import com.codewithmosh.store.exceptions.PaymentException;
 import com.codewithmosh.store.services.CartService;
 import com.codewithmosh.store.services.CheckoutService;
 import com.stripe.exception.StripeException;
@@ -26,19 +27,20 @@ public class CheckoutController {
 
 
     @PostMapping
-    public ResponseEntity<?> checkout(@Valid @RequestBody CheckoutRequest request)
+    public CheckoutResponse checkout (@Valid @RequestBody CheckoutRequest request)
     {
-        try
-        {
-        return  ResponseEntity.ok(checkoutService.checkout(request));
-        }
-        catch (StripeException ex)
-        {
-            return ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ErrorDto("Error creating a checkout session"));
-        }
+        return checkoutService.checkout(request);
     }
+
+    @ExceptionHandler(PaymentException.class)
+    public ResponseEntity<?> handlePaymentException()
+    {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorDto("Error creating a checkout session"));
+    }
+
+
     @ExceptionHandler({CartNotFoundException.class , CartEmptyException.class})
     public ResponseEntity<ErrorDto> handleException(Exception ex)
     {
---
+++ src/main/java/com/codewithmosh/store/exceptions/PaymentException.java
@@ -0,0 +1,8 @@
+package com.codewithmosh.store.exceptions;
+
+public class PaymentException extends RuntimeException {
+//
+//    public PaymentException() {
+//        super("Payment Failed");
+//    }
+}
