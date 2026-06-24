c1c565d6e97e37a8a2c555138fa032969b4006f8
mazen khaled <khaledmazen64@gmail.com>
2026-06-24 15:23:00 +0300

    update the status of the orders according to the payment_intetn.succeeded/fail

--- src/main/java/com/codewithmosh/store/controllers/CheckoutController.java
+++ src/main/java/com/codewithmosh/store/controllers/CheckoutController.java
@@ -3,13 +3,16 @@ package com.codewithmosh.store.controllers;
 import com.codewithmosh.store.dtos.CheckoutRequest;
 import com.codewithmosh.store.dtos.CheckoutResponse;
 import com.codewithmosh.store.dtos.ErrorDto;
+import com.codewithmosh.store.entities.OrderStatus;
 import com.codewithmosh.store.exceptions.CartEmptyException;
 import com.codewithmosh.store.exceptions.CartNotFoundException;
 import com.codewithmosh.store.exceptions.PaymentException;
+import com.codewithmosh.store.repositories.OrderRepository;
 import com.codewithmosh.store.services.CartService;
 import com.codewithmosh.store.services.CheckoutService;
 import com.stripe.exception.SignatureVerificationException;
 import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import com.stripe.net.Webhook;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -31,6 +34,7 @@ public class CheckoutController {
     private String webhookSecretKey;
 
     private final CheckoutService checkoutService;
+    private final OrderRepository orderRepository;
 
 
     @PostMapping
@@ -50,11 +54,20 @@ public class CheckoutController {
             System.out.println(event.getType());
 
             var stripeObject = event.getDataObjectDeserializer().getObject().orElse(null) ;
-            // charge -> (charge) stripeObject
-            // payment_intent.successeded -> (paymentIntent) stripeObject
+
             switch (event.getType()) {
                 case "payment_intent.successeded" -> {
+
                     // update order status ( PAID )
+                    var paymentIntent = (PaymentIntent) stripeObject;
+                    if(paymentIntent != null)
+                    {
+                        var order_id = paymentIntent.getMetadata().get("order_id") ;
+                        var order = orderRepository.findById(Long.valueOf(order_id)).orElseThrow() ;
+                        order.setStatus(OrderStatus.PAID);
+                        orderRepository.save(order);
+                    }
+
                 }
                 case "payment_intent.failed" -> {
                     //update order status ( FAILED )
